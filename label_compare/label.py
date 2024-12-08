c9c93481fc371e2a877fe03dc9973e5985370047
lesguillemets <kgkacko+github@gmail.com>
2024-12-09 01:54:58 +0900

    initial structure

---
+++ label_compare/label.py
@@ -0,0 +1,5 @@
+class Labelling(object):
+    def __init__(self):
+        pass
+    def say_hi(self):
+        print("HI")
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from label_compare.label import Labelling
+
+def main():
+    l = Labelling()
+    l.say_hi()
+
+if __name__ == "__main__":
+    main()
