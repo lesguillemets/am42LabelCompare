e76654bf7cac9e9249f3bd441cc04105dab302f9
lesguillemets <kgkacko+github@gmail.com>
2024-12-09 02:04:58 +0900

    fm

--- label_compare/label.py
+++ label_compare/label.py
@@ -1,5 +1,9 @@
+# label = (start, end (in seconds), label)
+type SingleLabelSecond = tuple[float, float, int]
+
+
 class Labelling(object):
-    def __init__(self):
-        pass
+    def __init__(self, ls: list[SingleLabelSecond]):
+        self.labels = ls
     def say_hi(self):
         print("HI")
