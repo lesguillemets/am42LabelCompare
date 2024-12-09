00b92a99488290a00e622e05ce627eef9f43948f
lesguillemets <kgkacko+github@gmail.com>
2024-12-09 12:38:43 +0900

    parsing

--- label_compare/label.py
+++ label_compare/label.py
@@ -1,9 +1,36 @@
-# label = (start, end (in seconds), label)
-type SingleLabelSecond = tuple[float, float, int]
+from __future__ import annotations
+
+import csv
+import math
+from pathlib import Path
+# label = (start, end (in frames), label)
+# the video is currently 29.97 fps
+
+VIDEO_FPS: float = 29.97
+type SingleLabel = tuple[int, int, int]
 
 
 class Labelling(object):
-    def __init__(self, ls: list[SingleLabelSecond]):
+    def __init__(self, ls: list[SingleLabel]) -> None:
         self.labels = ls
     def say_hi(self):
         print("HI")
+
+    @staticmethod
+    def from_csv_in_seconds(fname: Path) -> Labelling:
+        # 秒*29.97 が必ずしも整数に近づかないので，
+        # 多分 js でとれる video.duration は
+        # フレームごとに離散の値ではないんだと思う
+        # …ので，ここは 29.97 かけて floor でフレームにすることに
+        with open(fname, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            assert(next(reader) == ["start", "end", "label"])
+            dat = [ (
+                        math.floor(float(s)*VIDEO_FPS),
+                        math.floor(float(e)*VIDEO_FPS),
+                        int(l)
+                    ) for (s,e,l) in reader
+                   ]
+            return Labelling(dat)
+
+
--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from label_compare.label import Labelling
 
 def main():
-    l = Labelling()
-    l.say_hi()
+    d = Labelling.from_csv_in_seconds("./examples/example.csv")
+    print(d.labels)
 
 if __name__ == "__main__":
     main()
