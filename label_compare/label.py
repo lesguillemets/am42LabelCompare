b0be722c3420fbba515e0309ce090bb256e4910a
lesguillemets <kgkacko+github@gmail.com>
2024-12-09 12:53:13 +0900

    format

--- label_compare/label.py
+++ label_compare/label.py
@@ -14,26 +14,27 @@ class Labelling(object):
     def __init__(self, name: str, ls: list[SingleLabel]) -> None:
         self.name = name
         self.labels = ls
+
     def say_hi(self):
         print("HI")
 
     @staticmethod
-    def from_csv_in_seconds(fl: Path, name:str = "") -> Labelling:
+    def from_csv_in_seconds(fl: Path, name: str = "") -> Labelling:
         # 秒*29.97 が必ずしも整数に近づかないので，
         # 多分 js でとれる video.duration は
         # フレームごとに離散の値ではないんだと思う
         # …ので，ここは 29.97 かけて floor でフレームにすることに
-        with open(fl, newline='') as csvfile:
+        with open(fl, newline="") as csvfile:
             if name == "":
                 name = fl.name
-            reader = csv.reader(csvfile, delimiter=',')
-            assert(next(reader) == ["start", "end", "label"])
-            dat = [ (
-                        math.floor(float(s)*VIDEO_FPS),
-                        math.floor(float(e)*VIDEO_FPS),
-                        int(l)
-                    ) for (s,e,l) in reader
-                   ]
+            reader = csv.reader(csvfile, delimiter=",")
+            assert next(reader) == ["start", "end", "label"]
+            dat = [
+                (
+                    math.floor(float(s) * VIDEO_FPS),
+                    math.floor(float(e) * VIDEO_FPS),
+                    int(l),
+                )
+                for (s, e, l) in reader
+            ]
             return Labelling(name, dat)
-
-
--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 from label_compare.label import Labelling
 from pathlib import Path
 
+
 def main():
     datadir = Path("./examples/")
-    dat = [Labelling.from_csv_in_seconds(f) for f in datadir.glob('label-data*.csv')]
+    dat = [Labelling.from_csv_in_seconds(f) for f in datadir.glob("label-data*.csv")]
     for d in dat:
         print(d.name)
         print(d.labels)
+
 
 if __name__ == "__main__":
     main()
