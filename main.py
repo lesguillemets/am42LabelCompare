6b6ec832e4e3620a2bed0c9cc30f0f2d7b5d62eb
lesguillemets <kgkacko+github@gmail.com>
2024-12-11 17:33:17 +0900

    better naming

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ from label_compare.label import Labelling
 from label_compare.compare import LabelComparer
 from label_compare.labelcomparers import *
 
+import re
+
 from pathlib import Path
+
+PATTERN_FNAME_NAME = re.compile(r"label-data_*(.*)\.csv")
+
+def fname_to_name(s:str) -> str:
+    if (m := PATTERN_FNAME_NAME.match(s)) is not None:
+        return m.group(1)
+    else:
+        return s
+
 
 
 def main():
     datadir = Path("./examples/")
-    dat = [Labelling.from_csv_in_seconds(f) for f in datadir.glob("label-data*.csv")]
+    dat = [
+            Labelling.from_csv_in_seconds(
+                f, name= fname_to_name(f.name)
+                ) for f in datadir.glob("label-data*.csv")
+            ]
     for d in dat:
         d.report_stats()
         print("\n_____________________")
